4538cda32369916a61018bd596970b5808e7ba4d
Jez-er <andrey.chernenko008@gmail.com>
2025-02-16 15:43:12 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,13 @@ const config: Config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://your-docusaurus-site.example.com',
+	url: 'https://jez-er.github.io',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/',
-
+	baseUrl: '/strix.docs/',
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'Astral Team', // Usually your GitHub org/user name.
+	organizationName: 'AstralTeam', // Usually your GitHub org/user name.
 	projectName: 'Strix.js', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
